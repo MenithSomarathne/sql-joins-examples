9ca7435f1bb7b254150586f1480b74ae108b05aa
Sachith Rangana1 <sachith2546@gmail.com>
2024-08-06 19:45:51 +0530

    Add student records

--- db-script.sql
+++ db-script.sql
@@ -3,6 +3,17 @@ CREATE TABLE student(
     name VARCHAR(100) NOT NULL
 );
 
+INSERT INTO student VALUES
+                        ("C001", "Menith"),
+                        ("C002", "Lahiru"),
+                        ("C003", "Sanduni"),
+                        ("C004", "Dilini"),
+                        ("C005", "Sachith"),
+                        ("C006", "Wimukthi"),
+                        ("C007", "Ruwan"),
+                        ("C008", "Saman"),
+                        ("C009", "Sandun"),
+                        ("C010", "Udara");
 
 
 CREATE TABLE program(
